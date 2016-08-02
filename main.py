2f9f1249ed3b79080089ce85a63bba9b2dd7913a
Demo User <demouser@MacBook-Pro.local>
2016-08-02 09:23:17 -0400

    s

--- main.py
+++ main.py
@@ -13,37 +13,42 @@ jinja_environment = jinja2.Environment(loader=
 
 
 
-class PizzaOrder(ndb.Model):
+class Student(ndb.Model):
 
-    crust = ndb.StringProperty(required=True)
-    size = ndb.StringProperty(required=True)
-    sauce = ndb.StringProperty(required=False)
-    cheese = ndb.StringProperty(required=False)
-    topings = ndb.StringProperty(required=False)
-
-class Customer(ndb.Model):
-    email = ndb.StringProperty(required=True)
     name = ndb.StringProperty(required=True)
     age = ndb.IntegerProperty(required=True)
-    address = ndb.StringProperty(required=True)
-    pizza_id = ndb.KeyProperty(required=True)
+    email = ndb.StringProperty(required=True)
+    school = ndb.StringProperty(required=True)
+    professor = ndb.StringProperty(required=True)
+    description = ndb.StringProperty(required=True)
 
-
-
+class Tutor(ndb.Model):
+    name = ndb.StringProperty(required=True)
+    age = ndb.IntegerProperty(required=True)
+    email = ndb.StringProperty(required=True)
+    subject = ndb.StringProperty(repeated=True)
+    school = ndb.StringProperty(required=True)
+    professor = ndb.StringProperty(required=True)
+    description = ndb.StringProperty(required=True)
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         user = users.get_current_user()
         if user:
-            greeting = ('Welcome, %s! (<a href="%s">sign out</a>)' %
-                        (user.nickname(), users.create_logout_url('/')))
-            self.response.out.write('%s' % greeting)
-            template = jinja_environment.get_template('templates/input_order.html')
-            self.response.write(template.render())
+            student_query = Student.query()
+            student_query = student_query.filter(Student.email == user.email())
+            student_data = student_query.fetch()
+            if student_data:
+                self.response.write(student_data[0].age)
+            else:
+                greeting = ('Welcome, %s! (<a href="%s">sign out</a>)' %
+                            (user.nickname(), users.create_logout_url('/')))
+                self.response.out.write('%s' % greeting)
+                template = jinja_environment.get_template('templates/input_order.html')
+                self.response.write(template.render())
         else:
             self.response.write('<a href="%s">Sign in or register</a>.' %
                         users.create_login_url('/'))
-
 
 
     def post(self):
@@ -52,20 +57,21 @@ class MainHandler(webapp2.RequestHandler):
                     (user.nickname(), users.create_logout_url('/')))
         user = users.get_current_user()
         self.response.out.write('%s' % greeting)
+        student1 = Student(name=self.request.get('name'), age=int(self.request.get('age')), email=user.email(), school=self.request.get('school'), professor=self.request.get('professor'), description=self.request.get('description'))
+        student1.put()
+        student_query = Student.query()
+        student_query = student_query.filter(Student.email == user.email())
+        student_data = student_query.fetch()
+        self.response.write(student_data[0].age)
         template = jinja_environment.get_template('templates/output_order.html')
         pizza_order = {
-          'crust_answer': self.request.get('crust'),
-          'size_answer': self.request.get('size'),
-          'sauce_answer': self.request.get('sauce'),
-          'cheese_answer': self.request.get('cheese'),
-          'topings_answer': self.request.get('topings')}
-        pizza_instance = PizzaOrder(crust=self.request.get('crust'),size=self.request.get('size'),sauce=self.request.get('sauce'),cheese=self.request.get('cheese'),topings=self.request.get('topings'))
-        key = pizza_instance.put()
-        pizza_order['order_number']=key.id()
+          'name': student_data[0].name,
+          'age': student_data[0].age,
+          'sauce_answer': student_data[0].email,
+          'cheese_answer': student_data[0].school,
+          'topings_answer': student_data[0].professor,
+          'topings_answer': student_data[0].description}
         self.response.write(template.render(pizza_order))
-        customer1 = Customer(email=user.email(),name=self.request.get('name'),age=int(self.request.get('age')),pizza_id=key,address=self.request.get('address'))
-        customer1.put()
-
 
 app = webapp2.WSGIApplication([
   ('/', MainHandler),
